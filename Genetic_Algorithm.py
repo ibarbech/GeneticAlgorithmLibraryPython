199344c4c30128ffec2a27ac813aa7319bf33067
ibarbech <ibarbech@alumnos.unex.es>
2016-12-04 18:32:02 +0100

    Added Roulette, generate_child_split, pull with float and help

--- Genetic_Algorithm.py
+++ Genetic_Algorithm.py
@@ -11,7 +11,8 @@ from time import *
 ROULETTE = 1
 FILL_NEXT_GENERATION = 2
 
-CHILD_FLIP=10
+CHILD_FLIP = 10
+CHILD_SPLIT = 11
 
 class B(Exception):
     pass
@@ -19,11 +20,21 @@ class B(Exception):
 class GenecticException(B):
     what = None
 
+class chormosoma_t():
+    def __init__(self,n_chromosomes,maxvalue = None,minvalue = 0,chromosomes_is_float = False,):
+        self.N_CHRO = n_chromosomes
+        if maxvalue is None:
+            self.MAXVALUE = n_chromosomes
+        else:
+            self.MAXVALUE = maxvalue
+        self.MINVALUE = minvalue
+        self.CHRO_IS_F = chromosomes_is_float
+
 class GeneticAlgorithm:
 
 
-    def __init__(self, n_chromosomes,fun_fitness, itmax=50, child_type=CHILD_FLIP, selection_type=FILL_NEXT_GENERATION,
-                 High_Low=False, size_pull=1000, porcent_elitism=10, porcent_mute = 2):
+    def __init__(self,fun_fitness, chromosomes, itmax = 50, child_type = CHILD_FLIP, selection_type = FILL_NEXT_GENERATION,
+                 High_Low = False, size_pull = 1000, porcent_elitism = 10, porcent_mute = 2):
 
         if selection_type not in [ROULETTE, FILL_NEXT_GENERATION]:
             a = GenecticException
@@ -50,19 +61,25 @@ class GeneticAlgorithm:
         self.__REVERSE = High_Low
         self.__SELECTION_TYPE = selection_type
         self.__SIZE_PULL = size_pull
-        self.__N_CHROMOSOMES = n_chromosomes
-        self.__pull=self.__Generate_pull()
-        self.__FUN_FITNESS=fun_fitness
-        self.__ITMAX=itmax
-        self.__TYPECHILD=child_type
-        self.__rangechild=(int)(self.__SIZE_PULL - self.__SIZE_PULL * self.__ELITISM)
-        self.__sizeelitism=(int)(self.__SIZE_PULL * self.__ELITISM)
+        self.__N_CHROMOSOMES = chromosomes.N_CHRO
+        self.__FUN_FITNESS = fun_fitness
+        self.__ITMAX = itmax
+        self.__TYPECHILD = child_type
+        self.__rangechild = (int)(self.__SIZE_PULL - self.__SIZE_PULL * self.__ELITISM)
+        self.__sizeelitism = (int)(self.__SIZE_PULL * self.__ELITISM)
+        self.__CHRO_IS_F = chromosomes.CHRO_IS_F
+        self.__CHRO_MAXVALUE = chromosomes.MAXVALUE
+        self.__CHRO_MINVALUE = chromosomes.MINVALUE
+        self.__pull = self.__Generate_pull()
 
     def __Generate_pull(self):
-        new_pull = [[[0 for x in range(self.__N_CHROMOSOMES)], 0, 0] for y in range(self.__SIZE_PULL)]
-        for i in range(0, self.__SIZE_PULL):
-            new_pull[i][0] = sample(range(self.__N_CHROMOSOMES), self.__N_CHROMOSOMES)
-        return new_pull
+        if self.__CHRO_IS_F is False:
+            ind = range(self.__CHRO_MINVALUE, self.__CHRO_MAXVALUE)
+            new_pull = [[sample(ind,self.__N_CHROMOSOMES), 0, 0] for y in range(self.__SIZE_PULL)]
+            return new_pull
+        else:
+            new_pull = [[[uniform(self.__CHRO_MINVALUE,self.__CHRO_MAXVALUE) for x in range(self.__N_CHROMOSOMES)], 0, 0] for y in range(self.__SIZE_PULL)]
+            return new_pull
 
     def __getKey(self, item):
         return item[1]
@@ -70,7 +87,7 @@ class GeneticAlgorithm:
     def __Get_Individuos(self, individual):
         if individual not in range(0, self.__SIZE_PULL):
             a = GenecticException
-            a.what = "Error: individual",individual," not existing, 0 <= individual < " + str(self.__SIZE_PULL)
+            a.what = "Error: individual",individual," not existing, 0 < = individual < " + str(self.__SIZE_PULL)
             raise a
             return -1
         return copy.copy(self.__pull[individual][0])
@@ -78,21 +95,13 @@ class GeneticAlgorithm:
     def __Set_Fittnes(self, individual, Fittnes):
         if individual not in range(0, self.__SIZE_PULL):
             a = GenecticException
-            a.what = "Error: individual",individual," not existing, 0 <= individual < " + str(self.__SIZE_PULL)
+            a.what = "Error: individual",individual," not existing, 0 < = individual < " + str(self.__SIZE_PULL)
             raise a
             exit(-1)
-        self.__pull[individual][1]=Fittnes
+        self.__pull[individual][1] = Fittnes
 
-    def __sort_pull(self, rever=False):
-        self.__pull.sort(key=self.__getKey, reverse=rever)
-
-    def __Generate_Child_flip(self, item):
-        child = copy.copy(item)
-        index = [randint(0, self.__N_CHROMOSOMES - 1), randint(0, self.__N_CHROMOSOMES - 1)]
-        index.sort()
-        revert = copy.copy(item[index[0]:index[1]])
-        child[index[0]:index[1]] = revert[::-1]
-        return child
+    def __sort_pull(self, rever = False):
+        self.__pull.sort(key = self.__getKey, reverse = rever)
 
     def __Generate_Child_flip(self, item):
         child = copy.copy(item)
@@ -102,38 +111,90 @@ class GeneticAlgorithm:
         child[index[0]:index[1]] = revert[::-1]
         return child
 
-    def __Generate_Child(self, item):
+    def __Generate_Child_split(self, item1,item2):
+        child1 = copy.copy(item1)
+        child2 = copy.copy(item2)
+        split = randint(1, self.__N_CHROMOSOMES - 1)
+        child1[0:split] = copy.copy(child2[0:split])                          #[item1[::split],item2[split::]]
+        child2[split:self.__N_CHROMOSOMES] = copy.copy(item1[split:self.__N_CHROMOSOMES])         #[item2[::split],item1[split::]]
+        return self.__Replace_repeated(child1,child2,split)
+
+    def __Replace_repeated(self,child1,child2,split):
+        for i in range(0, split):
+            if child1[i] in child1[split:self.__N_CHROMOSOMES]:
+                for j in range(0, split):
+                    if child2[j] in child2[split:self.__N_CHROMOSOMES]:
+                        c1 = child1[i]
+                        child1[i] = child2[j]
+                        child2[j] = c1
+                        break
+        return [child1,child2]
+
+    def __Generate_Child(self, item1,item2 = None):
         if self.__TYPECHILD is CHILD_FLIP:
-            return self.__Generate_Child_flip(item)
+            return self.__Generate_Child_flip(item1)
+        elif self.__TYPECHILD is CHILD_SPLIT:
+            return self.__Generate_Child_split(item1,item2)
 
     def __Fill_Next_Generation(self):
-        new_pull=copy.copy(self.__pull)
-        for i in (range(0, self.__rangechild)):
-            child = self.__Generate_Child(copy.copy(self.__pull[i][0]))
-            dest = i + self.__sizeelitism
-            if randint(0, 99) < self.__MUTE:
-                child = self.__mute_individuals(child)
-            new_pull[dest][0] = copy.copy(child)
+        new_pull = copy.copy(self.__pull)
+        if self.__TYPECHILD is CHILD_FLIP:
+            for i in (range(0, self.__rangechild)):
+                child = self.__Generate_Child(copy.copy(self.__pull[i][0]))
+                dest = i + self.__sizeelitism
+                if randint(0, 99) < self.__MUTE:
+                    child = self.__mute_individuals(child)
+                new_pull[dest][0] = copy.copy(child)
+        elif self.__TYPECHILD is CHILD_SPLIT:
+            for i in (range(0, self.__rangechild,2)):
+                childs = self.__Generate_Child(copy.copy(self.__pull[i][0]),item2 = copy.copy(self.__pull[i+1][0]))
+                dest1 = i + self.__sizeelitism
+                dest2 = dest1+1
+                if randint(0, 99) < self.__MUTE:
+                    childs[0] = self.__mute_individuals(childs[0])
+                if randint(0, 99) < self.__MUTE:
+                    childs[1] = self.__mute_individuals(childs[1])
+                new_pull[dest1][0] = copy.copy(childs[0])
+                new_pull[dest2][0] = copy.copy(childs[1])
         return new_pull
 
     def __Roulette(self):
         new_pull = copy.copy(self.__pull)
-        for i in (range(1, self.__rangechild)):
-            self.__pull[i][1]=self.__pull[i][1]+self.__pull[i-1][1]
+        for i in (range(1, self.__SIZE_PULL)):
+            self.__pull[i][1] = self.__pull[i][1]+self.__pull[i-1][1]
         for i in (range(0, self.__SIZE_PULL)):
-            self.__pull[i][2]=1 - (self.__pull[i][1]/self.__pull[self.__SIZE_PULL-1][1])
+            self.__pull[i][2] = 1 - (self.__pull[i][1]/self.__pull[self.__SIZE_PULL-1][1])
 
-        for i in (range(0, self.__rangechild)):
-            selec=random()
-            ant = 0
-            for x in (range(0,self.__SIZE_PULL)):
-                if ant < selec and selec <self.__pull[i][2]:
-                    child = self.__Generate_Child(copy.copy(self.__pull[i][0]))
-                ant=self.__pull[i][2]
-            dest = i + self.__sizeelitism
-            if randint(0, 99) < self.__MUTE:
-                child = self.__mute_individuals(child)
-            new_pull[dest][0] = copy.copy(child)
+        if self.__TYPECHILD is CHILD_FLIP:
+            for i in (range(0, self.__rangechild)):
+                selec = random()
+                for j in range(0,self.__SIZE_PULL)[::-1]:
+                    if selec <self.__pull[j][2]:
+                        break
+                child = self.__Generate_Child(copy.copy(self.__pull[j][0]))
+                dest = i + self.__sizeelitism
+                if randint(0, 99) < self.__MUTE:
+                    child = self.__mute_individuals(child)
+                new_pull[dest][0] = copy.copy(child)
+        elif self.__TYPECHILD is CHILD_SPLIT:
+            for i in (range(0, self.__rangechild,2)):
+                selec1 = random()
+                selec2 = random()
+                for j in range(0, self.__SIZE_PULL)[::-1]:
+                    if selec1 < self.__pull[j][2]:
+                        break
+                for k in range(0, self.__SIZE_PULL)[::-1]:
+                    if selec2 < self.__pull[k][2]:
+                        break
+                childs = self.__Generate_Child(copy.copy(self.__pull[j][0]),copy.copy(self.__pull[k][0]))
+                dest1 = i + self.__sizeelitism
+                dest2 = dest1+1
+                if randint(0, 99) < self.__MUTE:
+                    childs[0] = self.__mute_individuals(childs[0])
+                if randint(0, 99) < self.__MUTE:
+                    childs[1] = self.__mute_individuals(childs[1])
+                new_pull[dest1][0] = copy.copy(childs[0])
+                new_pull[dest2][0] = copy.copy(childs[1])
         return new_pull
 
     def __Next_Generation(self):
@@ -153,6 +214,16 @@ class GeneticAlgorithm:
     def __mute_individuals_simple(self, item):
         return sample(item, self.__N_CHROMOSOMES)
 
+    def set_pull(self, pull):
+        self.__pull = copy.copy(pull)
+
+    def get_empty_pull(self):
+        pull = [[[0 for x in range(self.__N_CHROMOSOMES)], 0, 0] for y in range(self.__SIZE_PULL)]
+        return pull
+
+    def get_pull(self):
+        return copy.copy(self.__pull)
+
     def print_individuals(self):
         for i in range(0, self.__SIZE_PULL):
             print self.__pull[i][:], ","
@@ -160,17 +231,38 @@ class GeneticAlgorithm:
     def Winner_Probability(self):
         return self.__pull[0][:]
 
-    def run(self):
-        self.__Next_Generation()
+    def run(self,print_best=0):
         for x in range(self.__ITMAX):
-            self.__Next_Generation()
-            print "\rThe winer is: ", self.Winner_Probability()[1],
             for i in range(self.__SIZE_PULL):
-                item=self.__Get_Individuos(i)
+                item = self.__Get_Individuos(i)
                 self.__Set_Fittnes(i, self.__FUN_FITNESS(item))
             self.__sort_pull(self.__REVERSE)
+            print "\rThe winer is: ", self.Winner_Probability()[1],
+            if print_best is not 0:
+                print "\rThe winer is: ", self.Winner_Probability()[1]
+                for x in range(0,print_best):
+                    print self.__pull[x][0]
+                sleep(1)
+                print ("\n" * 100)
+            else :
+                print "\rThe winer is: ", self.Winner_Probability()[1],
+            self.__Next_Generation()
+
 
         print "\rThe winer is: ",self.Winner_Probability()
+
+    def help(self):
+        s = """GeneticAlgorithm
+        The structure of a tuple of the PULL is:
+        [individual, FITNNES, PROBABILITY FOR THE ROULETTE]
+
+        The structure of a individual is:
+        [value,value,value,...,value] It has as many values as chromosomes.
+
+
+
+
+        """
 
 
 
